098ac4564d45da86bbff2acc97349648f4781063
AlgoClassWork <kyxec.play@gmail.com>
2026-05-17 16:57:19 +0600

    Update shooter_game.py

--- shooter_game.py
+++ shooter_game.py
@@ -12,13 +12,21 @@ class GameSprite(sprite.Sprite):
     
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < 615:
+            self.rect.x += self.speed
     
 window = display.set_mode((700, 500))
 display.set_caption('Шутер')
 
 background = transform.scale(image.load('galaxy.jpg'), (700, 500))
 
-player = GameSprite('rocket.png', 5, 400, 80, 100, 10)
+player = Player('rocket.png', 5, 400, 80, 100, 10)
 enemy = GameSprite('ufo.png', 600, 100, 80, 50, 10)
 
 clock = time.Clock()
@@ -32,6 +40,8 @@ while True:
     window.blit(background, (0, 0))
     player.reset()
     enemy.reset()
+
+    player.update()
 
     display.update()
     clock.tick(60)
