472c16f2b6eed1b0ada1b0e32d30620aebc0877b
AlgoClassWork <kyxec.play@gmail.com>
2026-05-17 16:53:26 +0600

    Add files via upload

---
+++ shooter_game.py
@@ -0,0 +1,37 @@
+#Создай собственный Шутер!
+from pygame import *
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+        self.speed = player_speed
+    
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+    
+window = display.set_mode((700, 500))
+display.set_caption('Шутер')
+
+background = transform.scale(image.load('galaxy.jpg'), (700, 500))
+
+player = GameSprite('rocket.png', 5, 400, 80, 100, 10)
+enemy = GameSprite('ufo.png', 600, 100, 80, 50, 10)
+
+clock = time.Clock()
+
+while True:
+
+    for e in event.get():
+        if e.type == QUIT:
+            quit()
+
+    window.blit(background, (0, 0))
+    player.reset()
+    enemy.reset()
+
+    display.update()
+    clock.tick(60)
